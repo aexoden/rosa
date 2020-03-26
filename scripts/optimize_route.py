c208cf8a3cdb12db1eb1f2e9046221f5888c4e93
Jason Lynch <jason@calindora.com>
2020-03-26 06:09:23 -0700

    Ensure the process limit is always at least one in the optimization wrapper.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- scripts/optimize_route.py
+++ scripts/optimize_route.py
@@ -228,7 +228,7 @@ class Optimizer(object):
                 estimated_memory = max(memory_values)
                 if len(memory_values) < 16:
                     estimated_memory = ((46 - len(memory_values)) / 30) * max(memory_values)
-                ideal_max_processes = int((self._args.max_memory * 2 - estimated_memory) / estimated_memory)
+                ideal_max_processes = max(1, int((self._args.max_memory * 2 - estimated_memory) / estimated_memory))
             else:
                 ideal_max_processes = 1
 
